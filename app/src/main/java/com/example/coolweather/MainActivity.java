74ff3dda48b5624054562e3e1b2ac17aaa29e203
wzj <979376522@qq.com>
2017-08-05 15:37:21 +0800

    加入创建数据库表和各项配置

--- app/src/main/java/com/example/coolweather/MainActivity.java
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private  String key = "5ebc06a0e92b4b4e88ba0e40859d7a8b";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
